68fc5e965d551fc2a541abe6eec6f03131ede2e8
Ibrahim Alghailani <i.alghailani@cckp.sa>
2020-12-10 17:49:22 +0300

    Make api stateless

--- src/main/kotlin/space/ibrahim/todo/WebSecurityConfiguration.kt
+++ src/main/kotlin/space/ibrahim/todo/WebSecurityConfiguration.kt
@@ -1,19 +1,23 @@
 package space.ibrahim.todo
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import kotlin.Throws
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.http.HttpMethod
-import java.lang.Exception
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 
 @Configuration
 class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
-        http.authorizeRequests {
-            it.mvcMatchers(HttpMethod.POST, "/api/v1/user").permitAll().
-                    anyRequest().hasRole("USER")
-        }.httpBasic()
         http.csrf().disable()
+            .httpBasic()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .mvcMatchers(HttpMethod.POST, "/api/v1/user")
+            .permitAll()
+            .anyRequest().hasRole("USER")
+            .and()
     }
 }
